3897acb53015baef4f70f92277ae63f0669d6d72
schwopo <lofskim@gmail.com>
2016-06-30 19:13:30 +0200

    Wir haben den server in TCP geschrieben, muss aber UDP sein
    Ich hab die beispiele aus der vorlesung in den kot kopiert, aber der
    beschwert sich mit ner BindException.

--- src/main/java/gui/ModelCustomer.java
+++ src/main/java/gui/ModelCustomer.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class ModelCustomer {
 
@@ -21,16 +23,51 @@ public class ModelCustomer {
 
 	private void askForTimeFromServer() {
 
-		try (Socket serverCon = new Socket("localhost", 6667);
-				InputStream in = serverCon.getInputStream();
-				OutputStream out = serverCon.getOutputStream()) {
-			byte[] dateBytes = new byte[1337];
-			in.read(dateBytes);
-			String date = new String(dateBytes, "UTF-8");
-			System.out.println(date);
-		} catch (IOException e) {
-			e.printStackTrace();
+		InetAddress ia = null;
+		try {
+			ia = InetAddress.getByName("localhost");
+		} catch (UnknownHostException e2) {
+			e2.printStackTrace();
 		}
+		// Socket für den Klienten anlegen
+		try (DatagramSocket dSocket = new DatagramSocket(6667);) {
 
+			try {
+				int i = 0;
+				while (i < 10) {
+					String command = "time";
+
+					byte buffer[] = null;
+					buffer = command.getBytes();
+
+					// Paket mit der Anfrage vorbereiten
+					DatagramPacket packet = new DatagramPacket(buffer,
+							buffer.length, ia, 3431);
+					// Paket versenden
+					dSocket.send(packet);
+
+					byte answer[] = new byte[1024];
+					// Paket für die Antwort vorbereiten
+					packet = new DatagramPacket(answer, answer.length);
+					// Auf die Antwort warten
+					dSocket.receive(packet);
+
+					System.out.println(new String(packet.getData(), 0, packet
+							.getLength()));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					i++;
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
