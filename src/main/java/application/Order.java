c7f68c7a821d7bbfba0e513897e48ed4772e324c
schwopo <lofskim@gmail.com>
2016-07-12 16:08:58 +0200

    Order um Name und Passwort erweitert

--- src/main/java/application/Order.java
+++ src/main/java/application/Order.java
@@ -3,6 +3,25 @@ import fpt.com.Product;
 public class Order extends ProductList implements fpt.com.Order {
 
 	private static final long serialVersionUID = -6631413930636044757L;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String password;
 
 	@Override
 	public double getSum() {
@@ -21,7 +40,7 @@ public class Order extends ProductList implements fpt.com.Order {
 		}
 		return quant;
 	}
-	
+
 	@Override
 	public String toString(){
 		String s = "";
