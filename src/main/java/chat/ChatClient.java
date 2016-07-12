59a44c2524103f5c9420bb9add6b804bc79a75d8
schwopo <lofskim@gmail.com>
2016-07-12 17:25:31 +0200

    Chatclient Methodenstubs gegeben

--- src/main/java/chat/ChatClient.java
+++ src/main/java/chat/ChatClient.java
@@ -1,5 +1,19 @@
 package chat;
 
+import java.rmi.RemoteException;
+
 public class ChatClient implements ClientService {
+
+	@Override
+	public void send(String msg) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
